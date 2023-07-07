885e8a0e11ea0a9796d4ef9917e7fcf89d00d930
Ben Mordue <benmordue@gmail.com>
2023-07-07 21:50:32 +0100

    graph path again

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -81,7 +81,7 @@ export default {
   methods: {
     loadGraphData() {
       axios
-        .get("graph.json")
+        .get("/graph.json")
         .then(response => {
           this.graph = response.data;
           this.selectedLevel1Node = this.startingNode;
