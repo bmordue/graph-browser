67bf61789d4c425aa76c9d97e4b415f17bd37e92
Ben Mordue <benmordue@gmail.com>
2023-07-08 21:52:56 +0100

    slide over to keep browsing

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -43,7 +43,8 @@ export default {
     return {
       graph: null,
       selectedLevel1Node: null,
-      selectedLevel2Node: null
+      selectedLevel2Node: null,
+      selectedLevel3Node: null
     };
   },
   computed: {
@@ -66,12 +67,12 @@ export default {
         .map(edge => this.graph.nodes.find(node => node.id === edge.target));
     },
     level3Nodes() {
-      if (!this.graph || !this.selectedLevel2Node) {
+      if (!this.graph || !this.selectedLevel3Node) {
         return [];
       }
 
       return this.graph.edges
-        .filter(edge => edge.source === this.selectedLevel2Node)
+        .filter(edge => edge.source === this.selectedLevel3Node)
         .map(edge => this.graph.nodes.find(node => node.id === edge.target));
     }
   },
@@ -91,15 +92,16 @@ export default {
         });
     },
     selectLevel1Node(nodeId) {
-      this.selectedLevel1Node = nodeId;
-      this.selectedLevel2Node = null;
-    },
-    selectLevel2Node(nodeId) {
+      //this.selectedLevel1Node = nodeId;
       this.selectedLevel2Node = nodeId;
     },
+    selectLevel2Node(nodeId) {
+      this.selectedLevel3Node = nodeId;
+    },
     selectLevel3Node(nodeId) {
-      this.selectedLevel1Node = nodeId;
-      this.selectedLevel2Node = null;
+      this.selectedLevel1Node = this.selectedLevel2Node;
+      this.selectedLevel2Node = this.selectedLevel3Node;
+      this.selectedLevel3Node = nodeId;
     }
   }
 };
