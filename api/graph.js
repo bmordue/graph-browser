421fe6e16edf89f83359cca96acd659f8510a535
Ben Mordue <benmordue@gmail.com>
2023-07-18 16:45:58 +0100

    use express for api

--- api/graph.js
+++ api/graph.js
@@ -1,4 +1,4 @@
-const http = require('http');
+const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
 
 const PORT = 3000;
@@ -6,46 +6,78 @@ const PORT = 3000;
 // Create a database connection
 const db = new sqlite3.Database('trains.db');
 
-// Create the server
-const server = http.createServer((req, res) => {
-  if (req.url === '/api/graph') {
-    // Fetch nodes and edges from the database
-    db.all('SELECT * FROM nodes', (err, nodes) => {
-      if (err) {
-        console.error('Error fetching nodes:', err);
-        res.statusCode = 500;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify({ error: 'Internal Server Error' }));
-        return;
-      }
 
-      db.all('SELECT * FROM edges', (err, edges) => {
+// Define the resolvers
+const root = {
+  getNode: ({ id }) => {
+    return new Promise((resolve, reject) => {
+      db.get('SELECT * FROM nodes WHERE id = ?', [id], (err, row) => {
         if (err) {
-          console.error('Error fetching edges:', err);
-          res.statusCode = 500;
-          res.setHeader('Content-Type', 'application/json');
-          res.end(JSON.stringify({ error: 'Internal Server Error' }));
-          return;
+          reject(err);
+        } else {
+          resolve(row);
         }
-
-        const graphData = {
-          nodes: nodes,
-          edges: edges,
-        };
-
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.end(JSON.stringify(graphData));
       });
     });
-  } else {
-    res.statusCode = 404;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Not Found');
-  }
+  },
+  getNodes: () => {
+    return new Promise((resolve, reject) => {
+      db.all('SELECT * FROM nodes', (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  },
+  getEdges: () => {
+    return new Promise((resolve, reject) => {
+      db.all('SELECT * FROM edges', (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  },
+};
+
+// Create the Express app
+const app = express();
+
+
+
+// Custom API endpoint for /api/node/:id
+app.get('/api/node/:id', (req, res) => {
+  const id = req.params.id;
+  db.get('SELECT * FROM nodes WHERE id = ?', [id], (err, row) => {
+    if (err) {
+      console.error('Error fetching node:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else if (!row) {
+      res.status(404).json({ error: 'Node not found' });
+    } else {
+      res.json(row);
+    }
+  });
+});
+
+// get a list of nodes connected to node with a given id
+app.get('/api/node/:id/children', (req, res) => {
+  const id = req.params.id;
+  db.all('SELECT * FROM nodes WHERE id IN (SELECT target FROM edges WHERE source = ?)', [id], (err, rows) => {
+    if (err) {
+      console.error('Error fetching children:', err);
+      res.status(500).json({ error: 'Internal Server Error' });
+    } else {
+      res.json(rows);
+    }
+  });
 });
 
 // Start the server
-server.listen(PORT, () => {
+app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
